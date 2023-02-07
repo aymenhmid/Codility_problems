41eb59dada12b78815cb0493cfcd0dd80126b99b
Aymen Hmid <48675135+aymenhmid@users.noreply.github.com>
2023-02-07 23:32:07 +0100

    Create PermMissingElem.py

---
+++ PermMissingElem.py
@@ -0,0 +1,9 @@
+def solution(A):
+    # Implement your solution here
+    if(len(A) == 0): return 1
+    elif(len(A) == 1):
+        if(A[0] == 1): return 2
+        else: return 1
+    else:
+        n, sum_val = len(A) + 1, sum(A)
+        return int(((n*(n+1)) / 2) - sum_val)
